392706d8f45e6bbeec7a0a38b8ed3836ab99a406
Faizan <faizan.zahid2500@gmail.com>
2021-08-24 23:23:22 +0500

    Remove comments from task-3/calculate.js

--- task-3/calculate.js
+++ task-3/calculate.js
@@ -1,26 +1,3 @@
-// class Calculator {
-//   constructor(num1, num2){
-//     this.num1 = num1;
-//     this.num2 = num2;
-//     };
-  
-//   add(num1, num2) {
-//     return num1 + num2;
-//   }
-
-//   subtraction(num1, num2) {
-//     num1 - num2;
-//   }
-
-//   multiplication(num1, num2) {
-//     return num1 * num2;
-//   }
-
-//   division(num1, num2){
-//     return num1 / num2;
-//   }
-// };
-
 const calculator = {
   addition: function(num1, num2) {
     return num1 + num2;
